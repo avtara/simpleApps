2fb552d7fed3b9a0b54086c302ee427befc704e3
avtara <avtarakhrisna1@gmail.com>
2019-10-31 20:16:05 +0700

    Initial commit

---
+++ app/src/main/java/com/chutzpah/barvolume/MainActivity.java
@@ -0,0 +1,87 @@
+package com.chutzpah.barvolume;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edtLength;
+    private EditText edtWidth;
+    private EditText edtHeight;
+    private Button btnCalculate;
+    private TextView tvResult;
+
+    private static final String STATE_RESULT = "state_result";
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_RESULT, tvResult.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtLength= findViewById(R.id.edt_length);
+        edtWidth=findViewById(R.id.edt_width);
+        edtHeight = findViewById(R.id.edt_height);
+        btnCalculate = findViewById(R.id.btn_calculate);
+        tvResult = findViewById(R.id.tv_result);
+
+        btnCalculate.setOnClickListener(this);
+
+        if (savedInstanceState != null){
+            String result = savedInstanceState.getString(STATE_RESULT);
+            tvResult.setText(result);
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_calculate){
+            String inputLength = edtLength.getText().toString().trim();
+            String inputWidth = edtWidth.getText().toString().trim();
+            String inputHeight = edtHeight.getText().toString().trim();
+
+            boolean isEmptyFields = false;
+            boolean isInvalidDouble = false;
+
+            if (TextUtils.isEmpty(inputLength)){
+                isEmptyFields = true;
+                edtLength.setError("Field ini jangan kosong ea");
+            }
+
+            if (TextUtils.isEmpty(inputWidth)){
+                isEmptyFields = true;
+                edtWidth.setError("Field ini jangan kosong ea");
+            }
+
+            if (TextUtils.isEmpty(inputHeight)){
+                isEmptyFields = true;
+                edtHeight.setError("Field Ini jangan kosong ea");
+            }
+
+            if (!isEmptyFields){
+                double volume = Double.valueOf(inputLength) * Double.valueOf(inputWidth) * Double.valueOf(inputHeight);
+                tvResult.setText(String.valueOf(volume));
+            }
+        }
+    }
+
+    private Double parseToDouble(String str){
+        try {
+            return Double.valueOf(str);
+        }catch (NumberFormatException e){
+            return null;
+        }
+    }
+}
